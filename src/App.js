071c453d84a588e028d9cf90002714b6a9b2eca8
Reynard Kasim <reynardkasim28@gmail.com>
2020-09-13 04:27:08 +1000

    Add css and App.js files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './jumbotron-narrow.css'
+//import './bootstrap.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+        <title>Home</title>
+      </head>
+
+      <body>
+
+        <div class="container">
+          <div class="header clearfix">
+            <nav>
+              <ul class="nav nav-pills pull-right">
+                <li role="presentation" class="active"><a href="#">Sign Up</a></li>
+                <li role="presentation" class="active"><a href="#">Sign In</a></li>
+              </ul>
+            </nav>
+            <h3 class="text-muted">Logo</h3>
+          </div>
+
+          <div class="jumbotron">
+            <h1>Favours Table</h1>
+          </div>
+
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">User</th>
+                <th scope="col">Owes this user</th>
+                <th scope="col">Item</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Kevin</td>
+                <td>Kelly</td>
+                <td>coffee</td>
+              </tr>
+            </tbody>
+          </table>
+
+          <footer class="footer">
+            <p>&copy; Adv Internet Programing</p>
+          </footer>
+        </div>
+
+      </body>
+      </html>
+    );
+  }
 }
 
 export default App;
