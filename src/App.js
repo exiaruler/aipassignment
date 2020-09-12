8ac14c5dd9c3f0d67c41a99c7eb8115a0b8a8149
Samuel Li <samuelgh.li@gmail.com>
2020-09-13 04:46:45 +1000

    add import of react router in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './jumbotron-narrow.css'
 //import './bootstrap.css'
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 class App extends React.Component {
   constructor(props) {
     super(props);
