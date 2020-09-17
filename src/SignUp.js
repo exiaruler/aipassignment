35e8b0868edba3905969047cec9bc29cc47bef84
lilylilyngo <13605973@student.uts.edu.au>
2020-09-17 17:30:57 +1000

    Login button works

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,43 +1,76 @@
 import React from 'react';
+import './bootstrap.css'
 
-function SignUp() {
-  return (
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import Login from './Login';
 
-    <div class="container">
-        <div class="inner">
-            <div class="left">
-                <h1>Logo</h1>
-                
-                <div class="content">
-                    <h1>Sign up</h1>
-                    <p>Please fill in details to create account.</p>
 
-                    <div class="fullName"><input type="text" name="fullName" id = "fullName" placeholder="Full name"/>
-                    </div>
-                    <div class="email"><input type="text" name="email" id = "email" placeholder="Email"/>
-                    </div>
-                    <div class="password"><input type="text" name="password" id = "password" placeholder="Password"/>
-                    </div>
-                    <div class="userName"><input type="text" name="userName" id = "userName" placeholder="Username"/>
-                    </div>
 
-                    <button>Sign up</button>
-                    
-                </div>
-            </div>
-            <div class="right">
-                <h1>Login</h1>
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
 
-                <p>Already have an account? Press the button below to login.</p>
+    }
 
-                <button >lOgin</button>
-                
-            </div>
+    render() {
+        return (
+            <html lang="en">
+                <head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        </div>
-    </div>
 
-  );
+                </head>
+
+                <body>
+                    <form className="d-flex mt-5">
+
+                        <div class="container">
+                            <div class="inner">
+                                <div class="left">
+                                    <h1>Logo</h1>
+
+                                    <div class="content">
+                                        <h1>Sign up</h1>
+                                        <p>Please fill in details to create account.</p>
+                                        <form className="d-flex mt-5">
+
+                                            <div class="fullName"><input type="text" name="fullName" id="fullName" placeholder="Full name" />
+                                            </div>
+                                            <div class="email"><input type="text" name="email" id="email" placeholder="Email" />
+                                            </div>
+                                            <div class="password"><input type="text" name="password" id="password" placeholder="Password" />
+                                            </div>
+                                            <div class="userName"><input type="text" name="userName" id="userName" placeholder="Username" />
+                                            </div>
+
+                                            <button>Sign up</button>
+                                        </form>
+
+                                    </div>
+                                </div>
+                                <div class="right">
+                                    <h1>Login</h1>
+
+                                    <p>Already have an account? Press the button below to login.</p>
+                                    <Link to="/login">
+                                    <button >lOgin</button>
+                                    </Link>
+                                </div>
+                                <Route path="/login" component={Login} />
+
+                            </div>
+                        </div>
+                    </form>
+                </body>
+            </html >
+
+        );
+    }
 }
 
 export default SignUp;
