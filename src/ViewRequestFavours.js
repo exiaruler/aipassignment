bdad9df142169b43f286e83174559ff6fb27e03f
Reynard Kasim <reynardkasim28@gmail.com>
2020-09-21 01:37:24 +1000

    Fix table

--- src/ViewRequestFavours.js
+++ src/ViewRequestFavours.js
@@ -4,29 +4,27 @@ class ViewRequestFavours extends React.Component {
     render() {
         return(
         <html lang="en">
-        <div>
-            <h2>Favours</h2>
-        </div>
-
-     
-        <table class="table table-striped">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">User</th>
-                <th scope="col">Owes this user</th>
-                <th scope="col">Item</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Kevin</td>
-                <td>Kelly</td>
-                <td>coffee</td>
-              </tr>
-            </tbody>
-          </table>
+          <h1>Favours</h1>
+          <body>
+            <table class="table table-striped">
+                <thead>
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">User</th>
+                    <th scope="col">Owes this user</th>
+                    <th scope="col">Item</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Kevin</td>
+                    <td>Kelly</td>
+                    <td>coffee</td>
+                  </tr>
+                </tbody>
+              </table>
+            </body>
         </html>
         );
     }
