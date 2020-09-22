ac190342126478b99052b525a0f3855fef7d231a
lilylilyngo <13605973@student.uts.edu.au>
2020-09-22 20:56:22 +1000

    User database queries -lily

--- server/index.js
+++ server/index.js
@@ -91,8 +91,6 @@ app.get("/owefavour/:id",async(req,res)=>{
 );
 
 
-
-
 //insert query to test your database connection using postman using sample table with no primary keys 
 
 app.post("/todos", async (req, res) => {
@@ -108,6 +106,77 @@ app.post("/todos", async (req, res) => {
       console.error(err.message);
     }
   });
+
+  app.post("/signup", async (req, res) => { // lily
+    console.log(req.body);// just for testing
+  
+    const { fullName, email, password, userName } = req.body;
+    try {
+      const user = await pool.query("SELECT * FROM userData WHERE user_name = $1", [
+        userName
+      ]);
+  
+      if (user.rows.length > 0) { 
+        return res.status(401).json("User already exist!");
+      }
+  
+  
+      const newUser = await pool.query( 
+        "INSERT INTO userData (user_fullname, user_email, user_password, user_name) VALUES ($1, $2, $3, $4) RETURNING *",
+        [fullName, email, password, userName]
+      );
+  
+      return res.json(newUser);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
+  });
+  
+  app.post("/login", async (req, res) => { // lily
+    const { userName, password } = req.body;
+  
+    try {
+      const user = await pool.query("SELECT * FROM userData WHERE user_name = $1", [
+        userName
+      ]);
+  
+      if (user.rows.length === 0) {
+        return res.status(401).json("Invalid Credential");
+      }
+  
+      // need a password bycryt 
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+  
+    }
+  });
+  
+  app.get("/editaccount", async (req, res) => { // lily
+    console.log(req.body);// just for testing
+    const {id}=req.params;
+    const { fullName, email, userName, old_password, new_password } = req.body;
+  
+    // need old password to change details
+    try {
+      const password = await pool.query("SELECT * FROM userData WHERE user_password = $1", [
+        old_password
+      ]);
+  
+      if (password.rows.length === 0) {
+        return res.status(401).json("Invalid Password"); // if old password matches with database
+      }
+        // if not null then update
+      const editUserAccount = await pool.query("UPDATE userData SET user_fullname =$1, user_name=$3, user_password=$4,  user_email =$2 WHERE user_id =$5",
+      [fullName, email, userName, new_password, id]);
+      res.json("Todo was updated!");
+    } catch (err) {
+      console.error(err.message);
+    }
+    
+  });
+  
 
 app.listen(5000,()=>{
     console.log("server has started on port 5000");
