32a2c1160cc693b48c80d0285b3faee9c3cf6547
lilylilyngo <13605973@student.uts.edu.au>
2020-09-22 20:51:04 +1000

    sql table

---
+++ sql_table/tables.sql
@@ -0,0 +1,41 @@
+CREATE DATABASE aipassingmentdata;
+
+
+CREATE TABLE owefavour (
+PRIMARY KEY  favourID INT ,
+userID INT ,
+username VARCHAR,
+title VARCHAR,
+favourdescription VARCHAR,
+rewards VARCHAR,
+recievinguserID  INT ,
+recievingusername VARCHAR,
+complete Boolean,
+favourimage Bytea,
+
+/*
+FOREIGN KEY (userID)
+FOREIGN KEY (recievinguserID)
+REFERENCES user (userID)
+*/
+); 
+
+INSERT INTO  owefavour (favourID,userID,username,title,favourdescription,rewards,recievinguserID,recievingusername,complete,favourimage) VALUES
+(1,1,'jesus','coffee owe','brought a coffee','coffee',2,'michael',false,'null');
+
+
+
+CREATE TABLE todo(
+  todo_id SERIAL PRIMARY KEY,
+  description VARCHAR(255)
+);
+
+CREATE TABLE userData(
+  user_ID SERIAL,
+  user_fullName VARCHAR(255) not null,
+  user_name VARCHAR(255) not null,
+  user_password VARCHAR(255) not null,
+  user_email VARCHAR(255) not null,
+  user_role VARCHAR(255) not null,
+  PRIMARY KEY (user_ID)
+)
