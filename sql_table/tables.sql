902266bb9c42e035d693953e6692ef7b6b594a63
Reynard Kasim <reynardkasim28@gmail.com>
2020-09-27 02:09:34 +1000

    Add favourRequest table

--- sql_table/tables.sql
+++ sql_table/tables.sql
@@ -22,7 +22,22 @@ REFERENCES userData(user_ID)
 INSERT INTO  owefavour (favourID,userID,username,title,favourdescription,rewards,recievinguserID,recievingusername,complete,favourimage) VALUES
 (1,1,'jesus','coffee owe','brought a coffee','coffee',2,'michael','null','null');
 
-
+CREATE TABLE favourRequest (
+  favourID INT,
+  user_ID INT,
+  user_name VARCHAR(255),
+  title VARCHAR(255),
+  favour_description VARCHAR(255),
+  rewards VARCHAR(255),
+  completing_UserID INT,
+  completing_Username VARCHAR,
+  favour_image Bytea,
+  complete_image Bytea,
+  CONSTRAINT fk_user
+  PRIMARY KEY (favourID),
+  FOREIGN KEY (user_ID)
+  REFERENCES userData(user_ID)
+);
 
 CREATE TABLE todo(
   todo_id SERIAL PRIMARY KEY,
