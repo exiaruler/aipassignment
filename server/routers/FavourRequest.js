7e877215120e30747682472a07db52271417b877
Reynard Kasim <reynardkasim28@gmail.com>
2020-10-11 22:46:03 +1100

    Make changes to favour request

--- server/routers/FavourRequest.js
+++ server/routers/FavourRequest.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const pool = require("../db");
+const fs = require('fs');
 const multer=require('multer');
+const auth = require("../middleware/authoriseUser"); //jwt token for user access 
+
 //creates destination for image files and gives them a unique ID 
 const storage = multer.diskStorage({
   destination: './uploads/',
