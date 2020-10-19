58c8d4365f696486c335674cc2dd08638f22b7b0
Vivian The <vivianthe2508@gmail.com>
2020-10-19 18:31:39 +1100

    added things to UpdateFavour.js

--- src/UpdateFavour.js
+++ src/UpdateFavour.js
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+//import { Json } from "sequelize/types/lib/utils";
+import {toast} from "react-toastify";
+//import { json } from "sequelize/types";
 
 // update favour
 // favour title, description, rewards
@@ -9,8 +12,88 @@ const UpdateFavour = ({ favour }) => {
     const [title, setTitle] = useState(favour.title);
     const [description, setDescription] = useState(favour.favour_description);
     const [rewards, setRewards] = useState(favour.rewards);
+    const [getallfavour, setAllFavour] = useState([]);
+    const [favourID, setFavourID] = useState(""); 
 
-    /*const updateTitle = async e => {
+    // get local item form ManageFavours from samuel
+    // change naming convention to apple
+
+    // method: "GET"
+    // get owe favours from favour_id through local storage
+    const getOweFavours = async (e) => {
+        try {
+            const response = await fetch ("http://localhost:5000/owe/getowefavour", {
+                method: "GET",
+                headers: {
+                    jwtToken: localStorage.jwtToken,
+                },
+            });
+            const jsonData = await response.json();
+            setFavourID(jsonData.favour_id);
+
+            if (jsonData.jwtToken){
+                //localStorage
+            }
+        } catch (error) {
+            console.error(error.message);
+        }
+    };
+    
+    // localStorage.setItem("favour_ID", id); --> call like this "localStorage.favour_ID"
+
+    
+    // POST --> update data into database
+    const onSubmitForm = async (e) => { // update
+        e.preventDefault();
+        try {
+          const body = { title, description, rewards };// add local storage "apple = localstorage."
+          const response = await fetch("http://localhost:5000/owe/updateOweFavour", {
+            method: "POST",
+            headers: {
+              jwtToken: localStorage.jwtToken,
+              "Content-type": "application/json",
+            },
+            body: JSON.stringify(body),
+          });
+          const parseRes = await response.json();
+    
+          if (parseRes.jwtToken) {
+            localStorage.setItem("jwtToken", parseRes.jwtToken);
+            favour(true);
+            toast.success("Update Successfully!");
+          } else {
+            //setAuth(false); not needed
+            toast.error(parseRes);
+          }
+        } catch (err) {
+          console.error(err.message);
+        }
+      };
+
+      useEffect(() => {
+        getOweFavours();
+      }, []); 
+
+
+    return (
+        <html lang="en">
+            <button
+                type="button"
+                class="btn btn-primary"
+                data-toggle="modal"
+                data-target={`#id${favour.favour_id}`}>
+                Edit
+            </button>
+
+            
+        </html>
+    )
+};
+
+export default UpdateFavour;
+
+
+/*const updateTitle = async e => {
         e.preventDefault();
         try {
             // send request to update title
@@ -59,114 +142,3 @@ const UpdateFavour = ({ favour }) => {
             console.error(error.message);
         }
     }*/
-
-
-    // get local item form ManageFavours from samuel
-    // change naming convention to apple
-
-    // method: "GET"
-    // get owe favours from favour_id through local storage
-    
-    
-
-    const onSubmitForm = async (e) => { // update
-        e.preventDefault();
-        try {
-          const body = { title, description, rewards };// add local storage "apple = localstorage."
-          const response = await fetch("http://localhost:5000/owe/updateOweFavour", {
-            method: "POST",
-            headers: {
-              jwtToken: localStorage.jwtToken,
-              "Content-type": "application/json",
-            },
-            body: JSON.stringify(body),
-          });
-          const parseRes = await response.json();
-    
-          if (parseRes.jwtToken) {
-            localStorage.setItem("jwtToken", parseRes.jwtToken);
-            setAuth(true);
-            toast.success("Edit Successfully!");
-          } else {
-            //setAuth(false); not needed
-            toast.error(parseRes);
-          }
-        } catch (err) {
-          console.error(err.message);
-        }
-      };
-      useEffect(() => {
-        getAllFavours();
-      }, []); // localStorage.setItem("favour_ID", id); --> call like thid "localStorage.favour_ID"
-
-
-    return (
-        <html lang="en">
-            <button
-                type="button"
-                class="btn btn-primary"
-                data-toggle="modal"
-                data-target={`#id${favour.favour_id}`}>
-                Edit
-            </button>
-
-            <div class="modal" id={`id${favour.favour_id}`} onClick = {() => setTitle(favour.title)}>
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h4 class="modal-title">Update Favour</h4>
-                            <button
-                                type="button"
-                                class="close"
-                                data-dismiss="modal"
-                                onClick = {() => setTitle(favour.title), setDescription(favour.description), setRewards(favour.rewards)}>&times;
-                        </button>
-                        </div>
-
-                        <div class="modal-body">
-                            <input type='text'
-                                className="form-control"
-                                value={title}
-                                onChange={e => setTitle(e.target.value)} />
-                        </div>
-
-                        <div class="modal-body">
-                            <input type='text'
-                                className="form-control"
-                                value={description}
-                                onChange={e => setDescription(e.target.value)} />
-                        </div>
-
-                        <div class="modal-body">
-                            <input type='text'
-                                className="form-control"
-                                value={rewards}
-                                onChange={e => setRewards(e.target.value)} />
-                        </div>
-
-                        <div class="modal-footer">
-                            <button
-                                type="button"
-                                class="btn btn-danger"
-                                data-dismiss="modal"
-                                onClick ={e => updateTitle(e) && updateDescription(e) && updateRewards(e)}>Update
-                                
-            </button>
-                        </div>
-
-                        <div class="modal-footer">
-                            <button
-                                type="button"
-                                class="btn btn-danger"
-                                data-dismiss="modal">Close
-            </button>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </html>
-    )
-};
-
-export default UpdateFavour;
