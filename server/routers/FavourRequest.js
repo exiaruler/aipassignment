feba9acb57cdd8efb6852aa5ec50395f150f12c4
lilylilyngo <13605973@student.uts.edu.au>
2020-10-20 18:25:03 +1100

    lily- fix last push2

--- server/routers/FavourRequest.js
+++ server/routers/FavourRequest.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const pool = require("../db");
-const fs = require('fs');
-const multer=require('multer');
+const fs = require("fs");
+const multer = require("multer");
 const createJWT = require("../functions/createJWT"); // lily
 const auth = require("../middleware/authoriseUser"); //jwt token for user access
 
-//creates destination for image files and gives them a unique ID 
+//creates destination for image files and gives them a unique ID
 const storage = multer.diskStorage({
   destination: "./uploads/",
   filename: function (req, file, cb) {
@@ -25,19 +25,26 @@ var upload = multer({ dest: "./uploads/" });
 // add favourRequest
 router.post("/addFavourRequest", auth, async (req, res) => {
   try {
-    var date = new Date;
-    const {title, favour_description, rewards} = req.body;
-    //get current date 
-    const favourDate = +date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear();
+    var date = new Date();
+    const { title, favour_description, rewards } = req.body;
+    //get current date
+    const favourDate =
+      date.getFullYear() + "/" + (date.getMonth() + 1) + "/" + date.getDate();
 
     console.log(title);
     const username = await pool.query(
       "SELECT * FROM userData WHERE user_id = $1",
       [req.user.id]
     );
-    const newFavourRequest = await pool.query( 
-      "INSERT INTO favourRequest(title, username, favour_description, rewards, date) VALUES($1, $2, $3, $4, $5) RETURNING *",
-      [title, username.rows[0].user_name, favour_description, rewards, favourDate]
+    const newFavourRequest = await pool.query(
+      "INSERT INTO favourRequest(title, user_name, favour_description, rewards, favourRequest_date) VALUES($1, $2, $3, $4, $5) RETURNING *",
+      [
+        title,
+        username.rows[0].user_name,
+        favour_description,
+        rewards,
+        favourDate,
+      ]
     );
     res.json(newFavourRequest);
     console.log("Favour added");
@@ -49,7 +56,7 @@ router.post("/addFavourRequest", auth, async (req, res) => {
 router.get("/getAllFavourRequest", async (req, res) => {
   try {
     const allFavourRequests = await pool.query(
-      "SELECT title, username, favour_description, rewards, date FROM favourRequest;"
+      "SELECT title, user_name, favour_description, rewards, favourrequest_date FROM favourRequest;"
     );
     res.json(allFavourRequests.rows);
   } catch (err) {
